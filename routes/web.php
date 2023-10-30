9cde8559076ed384bf7ab7f3a524df0a882f95ba
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-30 19:04:57 +0700

    Blade Templating Engine - Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Yudha Prasetya",
         "email" => "yudha.213040077@mail.unpas.ac.id",
         "gambar" => "SEM5-Mancing.png"
@@ -26,5 +29,51 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "Judul Post Pertama",
+            "autor" => "Yudha Prasetya",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "Judul Post Kedua",
+            "autor" => "Yudha Prasetya",
+            "Body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis ullam quaerat iure harum officia mollitia porro itaque illum esse ut laboriosam ipsa in suscipit, voluptate inventore officiis blanditiis vero laborum obcaecati sed doloribus. Consectetur impedit facere, nobis quos molestiae sint. Ab nesciunt atque dolor, alias aut laboriosam earum ipsum libero, veniam eligendi sint illo sapiente nobis voluptatibus praesentium cupiditate autem mollitia in quae dolores exercitationem. Nemo unde provident tempore expedita, et eaque quo recusandae voluptatum placeat pariatur tempora aliquam possimus modi, minima, quibusdam dolorum eius nostrum odit amet est sapiente. Veniam libero necessitatibus hic dolores recusandae. Soluta reprehenderit perferendis veniam?"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "Judul Post Pertama",
+            "autor" => "Yudha Prasetya",
+            "Body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, dolorem voluptate saepe aperiam, quam aliquam hic, sit sint doloribus doloremque ratione delectus iure perferendis praesentium fugit dolores repellat! Laboriosam qui vel quae, error assumenda magni aperiam quibusdam dolore consequatur laborum reprehenderit expedita, cum labore ipsa commodi autem nostrum, quam molestiae."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "Judul Post Kedua",
+            "autor" => "Yudha Prasetya",
+            "Body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, dolorem voluptate saepe aperiam, quam aliquam hic, sit sint doloribus doloremque ratione delectus iure perferendis praesentium fugit dolores repellat! Laboriosam qui vel quae, error assumenda magni aperiam quibusdam dolore consequatur laborum reprehenderit expedita, cum labore ipsa commodi autem nostrum, quam molestiae."
+        ]
+    ];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
