f51cef833326e579064b480fd3faef6b438fe312
YudhaPew <yudhaprasetya678@gmail.com>
2023-10-30 18:09:28 +0700

    Struktur Folder, Routes, View - Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Yudha Prasetya",
+        "email" => "yudha.213040077@mail.unpas.ac.id",
+        "gambar" => "SEM5-Mancing.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
