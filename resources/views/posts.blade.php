ee37a1124e57503e809d7e36b8e8d27038864826
YudhaPew <yudhaprasetya678@gmail.com>
2023-11-04 14:46:41 +0700

    Problem N+1 - Laravel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section('container')
+<h1 class="mb-5">{{$title}}</h1>
 @foreach ($posts as $post)
 <article class="mb-5 pb-4 border-bottom">
   <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
